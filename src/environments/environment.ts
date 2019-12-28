ed10a77384add61878a85ec4fa294dee104d4fdd
gchiesap <gchiesap@gmail.com>
2019-12-28 11:30:12 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,15 @@
 
 export const environment = {
   production: false,
+  //Aca agrega las credenciales de firebase de tu cuenta
   firebase: {
-    apiKey: "AIzaSyBvLxOY4zp_GbzExDev8SJ78W6yOlx1iFk",
-    authDomain: "gianlaycaco.firebaseapp.com",
-    databaseURL: "https://gianlaycaco.firebaseio.com",
-    projectId: "gianlaycaco",
-    storageBucket: "",
-    messagingSenderId: "734519745739",
-    appId: "1:734519745739:web:cdd10e6886c8a29a6a0f4e"
+    apiKey: ,
+    authDomain: ,
+    databaseURL: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
   }
 };
 
